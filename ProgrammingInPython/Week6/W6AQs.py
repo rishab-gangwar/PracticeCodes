9a2361325a05839f9966e146a63aaf62bc414e37
Rishabh gangwar <64923428+rishab-gangwar@users.noreply.github.com>
2021-07-27 13:54:33 +0530

    Create W6AQs.py
    added my answers for Week 6 Activity Questions

---
+++ ProgrammingInPython/Week6/W6AQs.py
@@ -0,0 +1,502 @@
+'''
+____________Scores Dataset__________________
+'''
+
+scores = [ { 'Chemistry': 78,
+    'CityTown': 'Erode',
+    'DateOfBirth': '7 Nov',
+    'Gender': 'M',
+    'Mathematics': 68,
+    'Name': 'Bhuvanesh',
+    'Physics': 64,
+    'SeqNo': 0,
+    'Total': 210}, { 'Chemistry': 91,
+    'CityTown': 'Salem',
+    'DateOfBirth': '3 Jun',
+    'Gender': 'M',
+    'Mathematics': 62,
+    'Name': 'Harish',
+    'Physics': 45,
+    'SeqNo': 1,
+    'Total': 198},{ 'Chemistry': 77,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '4 Jan',
+    'Gender': 'M',
+    'Mathematics': 57,
+    'Name': 'Shashank',
+    'Physics': 54,
+    'SeqNo': 2,
+    'Total': 188},{ 'Chemistry': 78,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '5 May',
+    'Gender': 'F',
+    'Mathematics': 42,
+    'Name': 'Rida',
+    'Physics': 53,
+    'SeqNo': 3,
+    'Total': 173},{ 'Chemistry': 89,
+    'CityTown': 'Madurai',
+    'DateOfBirth': '17 Nov',
+    'Gender': 'F',
+    'Mathematics': 87,
+    'Name': 'Ritika',
+    'Physics': 64,
+    'SeqNo': 4,
+    'Total': 240},{ 'Chemistry': 84,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '8 Feb',
+    'Gender': 'F',
+    'Mathematics': 71,
+    'Name': 'Akshaya',
+    'Physics': 92,
+    'SeqNo': 5,
+    'Total': 247},{ 'Chemistry': 87,
+    'CityTown': 'Ambur',
+    'DateOfBirth': '23 Mar',
+    'Gender': 'M',
+    'Mathematics': 81,
+    'Name': 'Sameer',
+    'Physics': 82,
+    'SeqNo': 6,
+    'Total': 250},{ 'Chemistry': 76,
+    'CityTown': 'Vellore',
+    'DateOfBirth': '15 Mar',
+    'Gender': 'M',
+    'Mathematics': 84,
+    'Name': 'Aditya',
+    'Physics': 92,
+    'SeqNo': 7,
+    'Total': 252},{ 'Chemistry': 51,
+    'CityTown': 'Bengaluru',
+    'DateOfBirth': '28 Feb',
+    'Gender': 'M',
+    'Mathematics': 74,
+    'Name': 'Surya',
+    'Physics': 64,
+    'SeqNo': 8,
+    'Total': 189},{ 'Chemistry': 73,
+    'CityTown': 'Bengaluru',
+    'DateOfBirth': '6 Dec',
+    'Gender': 'M',
+    'Mathematics': 63,
+    'Name': 'Clarence',
+    'Physics': 88,
+    'SeqNo': 9,
+    'Total': 224},{ 'Chemistry': 68,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '12 Jan',
+    'Gender': 'F',
+    'Mathematics': 64,
+    'Name': 'Kavya',
+    'Physics': 72,
+    'SeqNo': 10,
+    'Total': 204},{ 'Chemistry': 92,
+    'CityTown': 'Bengaluru',
+    'DateOfBirth': '30 Apr',
+    'Gender': 'M',
+    'Mathematics': 97,
+    'Name': 'Rahul',
+    'Physics': 92,
+    'SeqNo': 11,
+    'Total': 281},{ 'Chemistry': 71,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '14 Jan',
+    'Gender': 'F',
+    'Mathematics': 52,
+    'Name': 'Srinidhi',
+    'Physics': 64,
+    'SeqNo': 12,
+    'Total': 187},{ 'Chemistry': 89,
+    'CityTown': 'Madurai',
+    'DateOfBirth': '6 May',
+    'Gender': 'M',
+    'Mathematics': 65,
+    'Name': 'Gopi',
+    'Physics': 73,
+    'SeqNo': 13,
+    'Total': 227},{ 'Chemistry': 93,
+    'CityTown': 'Trichy',
+    'DateOfBirth': '23 July',
+    'Gender': 'F',
+    'Mathematics': 89,
+    'Name': 'Sophia',
+    'Physics': 62,
+    'SeqNo': 14,
+    'Total': 244},{ 'Chemistry': 90,
+    'CityTown': 'Theni',
+    'DateOfBirth': '22 Sep',
+    'Gender': 'F',
+    'Mathematics': 76,
+    'Name': 'Goutami',
+    'Physics': 58,
+    'SeqNo': 15,
+    'Total': 224},{ 'Chemistry': 43,
+    'CityTown': 'Trichy',
+    'DateOfBirth': '30 Dec',
+    'Gender': 'M',
+    'Mathematics': 87,
+    'Name': 'Tauseef',
+    'Physics': 86,
+    'SeqNo': 16,
+    'Total': 216},{ 'Chemistry': 67,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '14 Dec',
+    'Gender': 'M',
+    'Mathematics': 62,
+    'Name': 'Arshad',
+    'Physics': 81,
+    'SeqNo': 17,
+    'Total': 210},{ 'Chemistry': 97,
+    'CityTown': 'Erode',
+    'DateOfBirth': '9 Oct',
+    'Gender': 'F',
+    'Mathematics': 72,
+    'Name': 'Abirami',
+    'Physics': 92,
+    'SeqNo': 18,
+    'Total': 261},{ 'Chemistry': 62,
+    'CityTown': 'Trichy',
+    'DateOfBirth': '30 Aug',
+    'Gender': 'M',
+    'Mathematics': 56,
+    'Name': 'Vetrivel',
+    'Physics': 78,
+    'SeqNo': 19,
+    'Total': 196},{ 'Chemistry': 91,
+    'CityTown': 'Vellore',
+    'DateOfBirth': '17 Sep',
+    'Gender': 'M',
+    'Mathematics': 93,
+    'Name': 'Kalyan',
+    'Physics': 68,
+    'SeqNo': 20,
+    'Total': 252},{ 'Chemistry': 74,
+    'CityTown': 'Bengaluru',
+    'DateOfBirth': '15 Mar',
+    'Gender': 'F',
+    'Mathematics': 78,
+    'Name': 'Monika',
+    'Physics': 69,
+    'SeqNo': 21,
+    'Total': 221},{ 'Chemistry': 57,
+    'CityTown': 'Nagercoil',
+    'DateOfBirth': '17 Jul',
+    'Gender': 'F',
+    'Mathematics': 62,
+    'Name': 'Priya',
+    'Physics': 62,
+    'SeqNo': 22,
+    'Total': 181},{ 'Chemistry': 88,
+    'CityTown': 'Bengaluru',
+    'DateOfBirth': '13 May',
+    'Gender': 'F',
+    'Mathematics': 97,
+    'Name': 'Deepika',
+    'Physics': 91,
+    'SeqNo': 23,
+    'Total': 276},{ 'Chemistry': 58,
+    'CityTown': 'Madurai',
+    'DateOfBirth': '26 Dec',
+    'Gender': 'M',
+    'Mathematics': 44,
+    'Name': 'Siddharth',
+    'Physics': 72,
+    'SeqNo': 24,
+    'Total': 174},{ 'Chemistry': 92,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '16 May',
+    'Gender': 'F',
+    'Mathematics': 87,
+    'Name': 'Geeta',
+    'Physics': 75,
+    'SeqNo': 25,
+    'Total': 254},{ 'Chemistry': 82,
+    'CityTown': 'Chennai',
+    'DateOfBirth': '22 Jul',
+    'Gender': 'M',
+    'Mathematics': 74,
+    'Name': 'JK',
+    'Physics': 71,
+    'SeqNo': 26,
+    'Total': 227},{ 'Chemistry': 52,
+    'CityTown': 'Madurai',
+    'DateOfBirth': '4 Mar',
+    'Gender': 'M',
+    'Mathematics': 81,
+    'Name': 'Jagan',
+    'Physics': 76,
+    'SeqNo': 27,
+    'Total': 209},{ 'Chemistry': 83,
+    'CityTown': 'Madurai',
+    'DateOfBirth': '10 Sep',
+    'Gender': 'F',
+    'Mathematics': 74,
+    'Name': 'Nisha',
+    'Physics': 83,
+    'SeqNo': 28,
+    'Total': 240},{ 'Chemistry': 81,
+    'CityTown': 'Vellore',
+    'DateOfBirth': '13 Oct',
+    'Gender': 'M',
+    'Mathematics': 72,
+    'Name': 'Naveen',
+    'Physics': 66,
+    'SeqNo': 29,
+    'Total': 219}]
+    
+#uncomment
+"""def remove():
+    L.pop() 
+L = list(range(5))
+print('before:', L)
+remove()
+print('after:', L)
+'''
+____________next problem__________________
+'''
+P = list(range(10))
+Q = P
+Q[0] = 100
+print(P == Q)
+print(P is Q)
+'''
+____________next problem__________________
+'''
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = A.copy()
+B[0][0] = 100
+print(A == B)
+print(A is B)
+'''
+____________next problem__________________
+'''
+
+import numpy as np
+D = dict()
+X1=[]
+Y1=[]
+X2=[]
+Y2=[]
+X3=[]
+Y3=[]
+for x in range(-100, 100):
+    for y in range(-100, 100):
+        if x ** 2 + y ** 2 - 1024 < 0:
+            D[(x, y)] = 'in'
+            X1.append(x)
+            Y1.append(y)
+        elif x **2 + y ** 2 - 1024 == 0:
+            D[(x, y)] = 'on'
+            X2.append(x)
+            Y2.append(y)
+        else:
+            D[(x, y)] = 'out' 
+            X3.append(x)
+            Y3.append(y)
+import matplotlib.pyplot as plt
+plt.scatter(X1,Y1,marker='*',c='b')
+plt.scatter(X2,Y2,marker='^',c='g')
+plt.scatter(X3,Y3,marker='o',c='r')
+'''
+____________next problem__________________
+'''
+mostcommon=0
+mostcommonNo=[]
+listFactors={}
+for i in range(1,101):
+  listFactors[i]=[]
+  for j in range(1,(i//2)+1):
+    if i%j==0:
+      listFactors[i].append(j)
+
+for i in range(1,101):
+  listFactors[i]=set(listFactors[i])
+for i in range(1,101):
+  print(i,listFactors[i])
+for i in range(1,101):
+  listFactors[i]=set(listFactors[i])
+for i in range(1,101):
+  for j in range(i+1,101):
+    if mostcommon<len(listFactors[i].intersection(listFactors[j])):
+      mostcommon=len(listFactors[i].intersection(listFactors[j]))
+      mostcommonNo=[i,j]
+print(mostcommon,mostcommonNo)
+'''
+____________next problem__________________
+'''
+
+yCord=[]
+dervATx=[]
+def CubicEqn(x):
+  return x**3-3*x**2+2*x-1
+def DerivCubicEqn(x):
+  return 3*x**2-6*x+2
+for i in range(-10000,10000):
+  yCord.append(CubicEqn(i/10))
+  dervATx.append(DerivCubicEqn(i/10))
+for i in range(1,19999):
+  if(dervATx[i-1]<0) and (dervATx[i]>0):
+    print("Y cordinates near local minima",yCord[i-1],yCord[i],yCord[i+1],(i-10000))
+    print("derivatives near local minima",dervATx[i-1],dervATx[i],dervATx[i+1],(i-10000))
+'''
+____________next problem__________________
+'''
+ListofNameDob=[]
+for i in scores:
+  name=''
+  dob=''
+  for j in i:
+    if j=='Name':
+      name=i[j]
+    if j=='DateOfBirth':
+      dob=i[j]
+  ListofNameDob.append((name,dob))
+for i in ListofNameDob:
+  print(i)
+
+'''
+____________next problem__________________
+'''
+PhysicsM=[]
+for students in scores:
+  PhysicsM.append(students['Physics'])
+print(set(PhysicsM),len(PhysicsM))
+'''
+____________next problem__________________
+'''
+CityGraph={}
+for student1 in scores:
+  CityGraph[student1["Name"]]=[]
+  for student2 in scores:
+    if (student1["CityTown"]==student2["CityTown"]) and (student1["Name"]!=student2["Name"]) :
+      CityGraph[student1["Name"]].append(student2["Name"])
+for i in CityGraph:
+  print(i," : ",CityGraph[i],"\n")
+
+'''
+____________next problem__________________
+'''
+def zero_matrix(dim):
+  mat=[]
+  for i in range(dim):
+    row=[]
+    for j in range(dim):
+      row.append(0)
+    mat.append(row)
+  return mat
+dim=len(scores)
+mat=zero_matrix(dim)
+for student1 in range(dim):
+  for student2 in range(dim):
+    if (scores[student1]["CityTown"]==scores[student2]["CityTown"]) and (scores[student1]["Name"]!=scores[student2]["Name"]) :
+      mat[student1][student2]=mat[student1][student2]=1
+def printmat(mat):
+  for i in mat:
+    for j in i:
+      print(j,end=' ')
+    print()
+printmat(mat)
+
+'''
+____________next problem__________________
+'''
+
+mathM=[]
+for students in scores:
+  mathM.append((students['Name'],students['Mathematics']))
+print((mathM),len(mathM))
+def insert(L,tup):
+  insertedL=[]
+  if len(L)==0:
+    insertedL.append(tup)
+    return insertedL
+  i=0
+  #print("  -",tup[1],L[i][1])
+  while tup[1]>L[i][1] and i<len(L)-1:
+    insertedL.append(L[i])
+    i+=1
+  insertedL.append(tup)
+  while len(insertedL)<len(L)and i<len(L)-1:
+    insertedL.append(L[i])
+    i+=1
+  print(insertedL)
+  return insertedL 
+
+def findmin(tupList):
+  min=101
+  mintup=tupList[0]
+  for i in tupList:      
+    if min>i[1]:
+      #print(i[1])
+      mintup=i
+      min=i[1]
+  return tuple(mintup)
+   
+def sort_(tupList):
+  sortedTupL=[]
+  while len(tupList)>0 :
+    mintup=findmin(tupList)
+    mint=[mintup]
+    sortedTupL+=(mint)
+    tupList.remove(mintup)
+  return sortedTupL
+mathM=sort_(mathM)
+print(mathM)
+
+'''
+____________next problem__________________
+'''
+
+import copy
+import random
+score_new=copy.deepcopy(scores)
+if score_new is not scores:
+  for i in score_new:
+    i['Biology']=random.randint(41,101)
+
+print(score_new)
+
+'''
+____________next problem__________________
+'''
+
+#Task1
+
+
+def distance(s1, s2):
+  return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1]) + abs(s1[2] - s2[2])
+data = [ ]
+size = len(scores)
+for si in range(size):
+  for sj in range(si + 1, size):
+    info = dict()
+    Task-3 
+    s1 = (scores[si]['Mathematics'], scores[si]['Physics'], scores[si] ['Chemistry'])
+    s2 = (scores[sj]['Mathematics'], scores[sj]['Physics'], scores[sj] ['Chemistry'])
+    dij = distance(s1, s2) 
+    info['S1'] = scores[si]['Name'] 
+    info['S2'] = scores[sj]['Name'] 
+    info['distance'] = dij 
+    data.append(info)
+
+#task2
+
+def insert(L, x): 
+  out_L = [ ] 
+  inserted = False 
+  for elem in L:
+  if (not inserted) and elem['distance'] > x['distance']: 
+    out_L.append(x) 
+    inserted = True 
+  out_L.append(elem) 
+  if not inserted: 
+    out_L.append(x) 
+  return out_L
+
+  #Task3 
+def isort(L): 
+  out_L = [ ] 
+  for elem in L: 
+    out_L = insert(out_L, elem) 
+  return out_L 
+sorted_data = isort(data) 
